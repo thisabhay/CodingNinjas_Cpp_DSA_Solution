cabb4ba79fdd6a14c7a96d6b85f82d79d7531411
Abhay Gupta <111756624+abhay-ctrl@users.noreply.github.com>
2022-12-28 21:09:18 +0530

    Operators & Loops

---
+++ Lecture 6/output6.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    for(int i = 0; i < 5; i = i + 1){
+    cout << i << " ";
+    i = i + 1;
+    }
+}
---
+++ Lecture 6/output7.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    for(int i = 1; i < 5; i = i + 1){
+    cout << i << " ";
+    i = i - 1;
+    }
+}
