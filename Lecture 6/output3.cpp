338aefe21b7b8b1c2b5f7d44ef08189f611bbe78
Abhay Gupta <111756624+abhay-ctrl@users.noreply.github.com>
2022-12-28 20:40:20 +0530

    Operators & Loop

---
+++ Lecture 6/output3.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+int main() 
+{        
+         int g = 3;
+         cout << (++g * 8);
+} 
---
+++ Lecture 6/output4.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int x = 10;
+    int y = 20;
+    if(x++ > 10 && ++y > 20 ){
+    cout << "Inside if ";
+    } else{
+    cout << "Inside else ";
+    }
+    cout << x << " " << y;
+}
