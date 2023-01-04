85c9ed516b04b1fd78860829b53263c2bd4366b0
Abhay Gupta <111756624+abhay-ctrl@users.noreply.github.com>
2023-01-04 23:57:50 +0530

    Quiz

---
+++ Lecture 6/Quiz3.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a = 10;
+    if(a > 5) {
+        int a = 100;
+    }
+    else {
+        int a = 110;
+    }
+    cout << a << endl;
+}
